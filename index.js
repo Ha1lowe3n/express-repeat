c882bade51c03990c80b782899bb5db119475cf6
Igor Trifonov <Ha1lowe3n@yandex.ru>
2022-03-18 20:53:49 +0800

    add post controller, add routes

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
 import morgan from "morgan";
+
 import { connectDB } from "./config/db.js";
+import postRouter from "./postRouter.js";
 
 dotenv.config();
 
 connectDB();
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 if (process.env.NODE_ENV === "development") {
     app.use(morgan("dev"));
 }
-
 app.use(express.json());
-
-// app.use("/api/users", userRouter);
-
-const PORT = process.env.PORT || 5000;
+app.use("/api/", postRouter);
 
 app.listen(PORT, console.log(`Server running on port ${PORT}`));
---
+++ postController.js
@@ -0,0 +1,65 @@
+import Post from "./postModel.js";
+
+class PostController {
+    async getAll(req, res) {
+        try {
+            const posts = await Post.find();
+            return res.json(posts);
+        } catch (error) {
+            return res.status(500).json(error);
+        }
+    }
+
+    async create(req, res) {
+        try {
+            const { author, title, content, picture } = req.body;
+            const post = await Post.create({ author, title, content, picture });
+            return res.json(post);
+        } catch (error) {
+            error.message = "Одно из полей не заполнено";
+            return res.status(500).json(error);
+        }
+    }
+
+    async getOne(req, res) {
+        try {
+            const post = await Post.findById(req.params.id);
+            return res.json(post);
+        } catch (error) {
+            return res.status(400).json(error);
+        }
+    }
+
+    async update(req, res) {
+        try {
+            const post = req.body;
+
+            for (const key of Object.keys(post)) {
+                if (key !== "author" && key !== "title" && key !== "content") {
+                    return res.status(400).json("Неверное поле");
+                }
+            }
+
+            const updatedPost = await Post.findByIdAndUpdate(
+                req.params.id,
+                post,
+                { new: true }
+            );
+            return res.json(updatedPost);
+        } catch (error) {
+            return res.status(500).json(error);
+        }
+    }
+
+    async delete(req, res) {
+        try {
+            console.log(req.body);
+            const deletedPost = await Post.findByIdAndDelete(req.params.id);
+            return res.json(deletedPost);
+        } catch (error) {
+            return res.status(500).json(error);
+        }
+    }
+}
+
+export default new PostController();
